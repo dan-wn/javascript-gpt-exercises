4580f23f6306f2d3fabfc8da9b1dfc4acdc1602d
Dawn <danmanalo.gg@gmail.com>
2026-02-19 06:03:15 +0800

    Add solution for exercise 1

---
+++ script.js
@@ -0,0 +1,14 @@
+// Exercise 1: Variable & Condition Thinking
+
+const age = 18;
+const hasID = true;
+
+const isAdult = age >= 18;
+const isVerified = hasID;
+
+if (isAdult && isVerified) {
+  console.log("Allowed");
+} 
+else {
+  console.log("Not Allowed");
+}
